57ecfd978879db4b16d3df74627c313715ac4fcd
C.T. Bell <charlestbell@gmail.com>
2020-12-09 22:18:10 -0500

    Adding Javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // variables
+  //Current time
+  //An object that holds the hour and the note for that hour
+  //Function that grabs the current time and stores it in currentTime
+  //A function that formats the current date and displays it in the heading row
+  //function that renders the contents of the hours to the screen
+  //For each loop
+  //Save each hour's identifier in a data-hour field
+  //A function that updates the Css colors for the hour divs to indicate past, present, future
+  //Click listener on the save buttons
+});
